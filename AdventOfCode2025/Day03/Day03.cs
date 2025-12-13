08f2f18b63633dc8a01a6601dfb469c1ae33da91
David Porter <st10137289@vcconnect.edu.za>
2025-12-13 09:49:46 +0200

    Day 03 puzzle started

---
+++ AdventOfCode2025/Day03/Day03.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2025.Day03;
+
+public class Day03
+{
+ public static void Run()
+    {
+        
+        var lines = File.ReadAllLines("AdventOfCode2025/Day03/Input/day3.txt");
+        foreach (var line in lines)
+        {
+            var battery = line;
+        }
+    }   
+}
